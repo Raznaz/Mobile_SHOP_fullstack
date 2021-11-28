f4e11340f3a9c90c2095ddc0589f347a003b29f4
Naz <nazzzarr@gmail.com>
2021-11-28 14:22:45 +0200

    finish models sequilize

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 require('dotenv').config();
+
+const sequelize = require('./db');
+const models = require('./models/models.js');
+
 const express = require('express');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+start();
